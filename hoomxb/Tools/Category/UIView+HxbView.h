2ae7ffec8bd64abbfff0be18c11df104e33b7f5f
lipengyue <pengyue.li.@hoomsun.com>
2017-04-12 20:30:35 +0800

    View

---
+++ hoomxb/Tools/Category/UIView+HxbView.h
@@ -0,0 +1,13 @@
+//
+//  UIView+HxbView.h
+//  hoomxb
+//
+//  Created by HXB on 2017/4/12.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HxbView)
+
+@end
---
+++ hoomxb/Tools/Category/UIView+HxbView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+HxbView.m
+//  hoomxb
+//
+//  Created by HXB on 2017/4/12.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "UIView+HxbView.h"
+
+@implementation UIView (HxbView)
+
+@end
