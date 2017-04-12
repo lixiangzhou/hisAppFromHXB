f873edf0894f3a29360fb0b707bdb97dcf0f4d38
刘鹏 <peng.liu@hoomsun.com>
2017-04-12 20:04:10 +0800

    category

---
+++ hoomxb/Tools/Category/UIViewController+HxbViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HxbViewController.h
+//  hoomxb
+//
+//  Created by HXB-C on 2017/4/12.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (HxbViewController)
+
+@end
---
+++ hoomxb/Tools/Category/UIViewController+HxbViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HxbViewController.m
+//  hoomxb
+//
+//  Created by HXB-C on 2017/4/12.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "UIViewController+HxbViewController.h"
+
+@implementation UIViewController (HxbViewController)
+
+@end
