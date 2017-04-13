0fbeea0d136e6f6ae4fa0ad98c9903a86b24a495
lipengyue <pengyue.li.@hoomsun.com>
2017-04-13 11:39:55 +0800

    category

--- hoomxb/Tools/Category/HxbCategorys.h
+++ hoomxb/Tools/Category/HxbCategorys.h
@@ -5,14 +5,75 @@
 //  Created by HXB on 2017/4/13.
 //  Copyright © 2017年 hoomsun-miniX. All rights reserved.
 
-//
+
+
+
+
+
+// ------------------ readMe --------------------
+/*
+ * 各种分类
+ 
+ */
+
+
+
+
+//主要方法： 子controller添加到父controller
 #import "UIViewController+HxbViewController"
+
+
+/*
+ 1. frame 的快捷获取及写入
+ 2. 屏幕截图
+ */
 #import "UIView+HxbView"
+
+/*
+ 1. log 打印中文
+ */
 #import "NSArray+HxbLog"
+
+/*
+ 1. 使用图像和文本生成上下排列的属性文本
+ */
 #import "NSAttributedString+HxbAttributedString"
+
+/*
+ 1. 获取对象的属性列表
+ */
 #import "NSObject+HxbRunTime"
+
+
+/*
+ 1.计算HMAC MD5散列结果
+ 2.计算HMAC SHA1散列结果
+ 3.计算HMAC SHA256散列结果
+ 4.计算HMAC SHA512散列结果
+ 5.等。。。。
+ */
 #import "NSString+HxbHash"
+
+
+/*
+ 1. button的快速创建
+ */
 #import "UIButton+HxbButton"
+
+/*
+ 1.使用 16 进制数字创建颜色，例如 0xFF0000 创建红色
+ 2.生成随机颜色
+ 3.使用 R / G / B 数值创建颜色
+ */
 #import "UIColor+HxbColor"
+
+
+/*
+ 1. label 的快速创建
+ */
 #import "UILabel+HxbLabel"
+
+/*
+ 1. 快速获取屏幕的宽，高，分辨率
+ */
 #import "UIScreen+Hxb"
