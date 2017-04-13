1e0aa60197e2390cefea850b3892d2568aea0666
lipengyue <pengyue.li.@hoomsun.com>
2017-04-13 21:07:07 +0800

    base

---
+++ hoomxb/Tools/Base/HXBBaseTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HXBBaseTableViewCell.h
+//  hoomxb
+//
+//  Created by HXB on 2017/4/13.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXBBaseTableViewCell : UITableViewCell
+
+@end
