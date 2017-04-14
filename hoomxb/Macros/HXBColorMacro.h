c174bfdb1ede706bf78acf56a5207323aceb0ccc
刘鹏 <peng.liu@hoomsun.com>
2017-04-14 14:44:24 +0800

    base

---
+++ hoomxb/Macros/HXBColorMacro.h
@@ -0,0 +1,58 @@
+//
+//  HXBColorMacro.h
+//  HongXiaoBao
+//
+//  Created by hoomsun on 16/8/8.
+//  Copyright © 2016年 hongxb. All rights reserved.
+//
+
+#ifndef HXBColorMacro_h
+#define HXBColorMacro_h
+
+
+// 色值库
+
+#define COR1    [UIColor colorWithRed:251/255.0f green:83/255.0f blue:83/255.0f alpha:1]    // 浅红
+
+#define COR2    [UIColor colorWithRed:60/255.0f green:170/255.0f blue:250/255.0f alpha:1]    // 浅蓝
+
+#define COR3    [UIColor colorWithRed:245/255.0f green:166/255.0f blue:35/255.0f alpha:1]    // 橘黄
+
+#define COR4    [UIColor colorWithRed:61/255.0f green:219/255.0f blue:176/255.0f alpha:1]    // 蓝绿
+
+// 黑系
+#define COR5    [UIColor colorWithRed:34/255.0f green:34/255.0f blue:34/255.0f alpha:1]    // 色
+#define COR6    [UIColor colorWithRed:51/255.0f green:51/255.0f blue:51/255.0f alpha:1]    // 色
+#define COR7    [UIColor colorWithRed:85/255.0f green:85/255.0f blue:85/255.0f alpha:1]    // 色
+
+// 深灰
+#define COR8    [UIColor colorWithRed:119/255.0f green:119/255.0f blue:119/255.0f alpha:1]    // 色
+#define COR9    [UIColor colorWithRed:136/255.0f green:136/255.0f blue:136/255.0f alpha:1]    // 色
+#define COR10    [UIColor colorWithRed:153/255.0f green:153/255.0f blue:153/255.0f alpha:1]    // 色
+// 中灰
+#define COR11    [UIColor colorWithRed:197/255.0f green:197/255.0f blue:197/255.0f alpha:1]    // 色
+// 浅灰
+#define COR12    [UIColor colorWithRed:224/255.0f green:224/255.0f blue:224/255.0f alpha:1]    // 色
+#define COR13    [UIColor colorWithRed:232/255.0f green:232/255.0f blue:232/255.0f alpha:1]    // 色
+#define COR14    [UIColor colorWithRed:240/255.0f green:240/255.0f blue:240/255.0f alpha:1]    // 色
+
+#define COR15    [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1]    // 白色
+
+#define COR16    [UIColor colorWithRed:254/255.0f green:163/255.0f blue:163/255.0f alpha:1]    // 粉色
+
+#define COR17    [UIColor colorWithRed:250/255.0f green:250/255.0f blue:250/255.0f alpha:1]    // 亮灰色
+
+#define COR18    [UIColor colorWithRed:229/255.0f green:64/255.0f blue:64/255.0f alpha:1]    // 红色
+
+#define COR19    [UIColor colorWithRed:37/255.0f green:152/255.0f blue:230/255.0f alpha:1]    // 蓝色
+
+#define COR20    [UIColor colorWithRed:255/255.0f green:141/255.0f blue:25/255.0f alpha:1]  // 首页消息背景橙色
+
+#define COR21    [UIColor colorWithRed:249/255.0f green:249/255.0f blue:249/255.0f alpha:1]
+
+#define COR22    [UIColor colorWithRed:240/255.0f green:75/255.0f blue:40/255.0f alpha:1]  // 新配色
+
+#define COR23    [UIColor colorWithRed:221/255.0f green:58/255.0f blue:22/255.0f alpha:1]
+
+
+#endif /* HXBColorMacro_h */
---
+++ hoomxb/Macros/MacrosInBusiness.h
@@ -0,0 +1,147 @@
+//
+//  MacrosInBusiness.h
+//  TR7TreesV3
+//
+//  Created by hoomsun on 16/6/16.
+//  Copyright © 2016年 hoomsun. All rights reserved.
+//
+
+#ifndef MacrosInBusiness_h
+#define MacrosInBusiness_h
+
+// 标的状态
+typedef NS_ENUM(NSInteger, ProductStatus) {
+    kProductStatusCreated,  //!< 已创建
+    kProductStatusCollecting,   //!<筹款中
+    kProductStatusRepaying,     //!<还款中
+    kProductStatusSelledOut,    //!<已售罄
+    kProductStatusAborted,      //!<流标
+    kProductStatusRepayed,      //!<已还款
+    kProductStatusRemoved       //!<撤标
+    
+};
+// 红利的状态
+typedef NS_ENUM(NSInteger, PlanStatus) {
+    kPlanStatusCreated,  //!< 已创建
+    kPlanStatusCollecting,   //!<筹款中
+    kPlanStatusRepaying,     //!<收益中
+    kPlanStatusSoldOut,      //!<已满额
+    kPlanStatusFailed,       //!<流标
+    kPlanStatusRepaid,       //!<已退出
+    kPlanStatusRemoved,       //!<撤标
+    kPlanStatusRepaidAHead,  //提前还款
+    kPlanStatusExiting         //退出中
+};
+//# 加入中、已满额、收益中、退出中，已退出
+//PLAN_STATUS = {
+//# CREATED: '已创建',
+//COLLECTING: '加入中',
+//SOLD_OUT: '已满额',
+//REPAYING: '收益中',
+//REPAID: '已退出',
+//# FAILED: '流标',
+//# REMOVED: '撤标',
+//# REPAID_AHEAD: '提前还款',
+//EXITING: '退出中',
+//    
+//}
+//CREATED = 0
+//COLLECTING = 1
+//SOLD_OUT = 3
+//REPAYING = 2
+//REPAID = 5
+//FAILED = 4
+//REMOVED = 6
+//REPAID_AHEAD = 7
+//EXITING = 8
+
+
+// 标的分类
+typedef NS_ENUM(NSInteger, ProductCategory) {
+    kProductCategoryHignQualityDebt,
+    kProductCategoryCarDebt,
+    kProductCategoryPlanning,
+    kProductCategoryRegularFinancial
+};
+typedef NS_ENUM(NSInteger, PayResultCategory) {
+    kFail,                              //!< 支付失败
+    kSuccess,                           //!< 成功
+    kWaitting,                          //!< 等待
+    kCancel                             //!< 取消
+    
+};
+typedef NS_ENUM(NSInteger, TopUpResultCategory) {
+    kTopUpWaitting,                     //!< 等待
+    kTopUpSuccess,                      //!< 成功
+    kTopUpFail,                         //!< 支付失败
+};
+typedef enum {
+    FreeTarget = 0,
+    CessionOfClaim,
+    PlanProduct
+}ProductType;
+//颜色
+//#define BACKGROUNDCOLOR [UIColor colorWithRed:247/255.0f green:248/255.0f blue:249/255.0f alpha:1]
+#define BACKGROUNDCOLOR COR14
+#define DETAIL_TEXT_COLOR [[UIColor alloc]initWithRed:131/255.0f green:131/255.0f blue:131/255.0f alpha:1]
+#define BACKITEM_COLOR  [UIColor colorWithRed:131/255.0f green:131/255.0f blue:131/255.0f alpha:1]
+#define TITLE_COLOR [UIColor colorWithRed:57/255.0f green:57/255.0f blue:57/255.0f alpha:1]
+
+// trade record
+#define ADD_COLOR [[UIColor alloc]initWithRed:55/255.0f green:167/255.0f blue:91/255.0f alpha:1]
+#define LESS_COLOR [[UIColor alloc]initWithRed:146/255.0f green:0 blue:0 alpha:1]
+
+//
+//主色调
+#define MAIN_THEME_COLOR [UIColor colorWithRed:251/255.0f green:91/255.0f blue:91/255.0f alpha:1]
+//蓝色主色调
+#define MAIN_THEME_BULECOLOR [UIColor colorWithRed:0/255.0f green:180/255.0f blue:255/255.0f alpha:1]
+//黑色字体主色调
+#define MAIN_THEME_TEXTCOLOR [UIColor colorWithRed:57/255.0f green:57/255.0f blue:57/255.0f alpha:1]
+//灰色线条色调
+#define GrayLineColor COR13
+//#define AssetNaviBarColor 
+
+#define kWindow   [UIApplication sharedApplication].keyWindow
+
+//一般的业务常量
+#define CusSerTel                       @"400-1551-888"
+
+
+//#define FAQUrl                          @"http://www.hoomxb.com/app-files/questions.html" //
+#define SecurityProtect                 @"https://www.hoomxb.com/app-files/security.html"
+#define AboutUs                         @"https://www.hoomxb.com/app-files/about-us.html"
+#define BankLimitUrl                    @"https://www.hoomxb.com/app-files/bank-limit.html"
+#define FAQUrl                          @"https://www.hoomxb.com/app-files/dist/template/info.html"
+//#define SecurityProtect                 @"http://www.hoomxb.com/app-files/security.html"
+
+#define LoanAgreement                   @"https://www.hoomxb.com/app-files/debt-conditions.html"
+//#define ServiceAttorneyLetter           @"http://www.hoomxb.com/contract/service-au.html"
+#define RegisterAgreement               @"https://www.hoomxb.com/app-files/register.html"
+//#define UseTermsAndPrivacyPolicy        @"http://www.hoomxb.com/contract/privacy.html"
+#define planConditions                  @"https://www.hoomxb.com/app-files/plan-conditions.html"
+
+//http://www.hoomxb.com/contract/loan — 出借咨询与服务协议 //
+//http://www.hoomxb.com/contract/service — 服务计划授权委托书(?自动转投服务协议)
+//http://www.hoomxb.com/contract/register — 红小宝用户协议 //
+//http://www.hoomxb.com/contract/privacy — 使用条款和隐私政策协议 //
+
+
+/**
+ (0 -> Product | 1 -> Develop)Fuiou Pay
+*/
+
+#if 1
+#define    MchntCD     @"0002900F0096235"
+#define    PayKey      @"5old71wihg2tqjug9kkpxnhx9hiujoqj"
+#define    isPayTest   1
+//Develop
+#else
+#define    MchntCD     @"0002900F0286298"
+#define    PayKey      @"6807d6d98f3533636eb117e6028a5c31"
+#define    isPayTest   0
+//Product
+#endif
+
+
+#endif /* MacrosInBusiness_h */
