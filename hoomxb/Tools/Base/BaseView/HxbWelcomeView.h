c73637bf6f7c6da15a69ae4725e2a72bdf8f6e3e
刘鹏 <peng.liu@hoomsun.com>
2017-04-18 18:58:58 +0800

    welcome

---
+++ hoomxb/Tools/Base/BaseView/HxbWelcomeView.h
@@ -0,0 +1,13 @@
+//
+//  HxbWelcomeView.h
+//  hoomxb
+//
+//  Created by HXB-C on 2017/4/18.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HxbWelcomeView : UIView
+
+@end
