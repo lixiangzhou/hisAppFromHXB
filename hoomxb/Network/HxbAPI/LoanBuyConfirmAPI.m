9ec0d081211fb0de6a67eddd2ce80b01e2fe454a
刘鹏 <peng.liu@hoomsun.com>
2017-04-18 10:22:42 +0800

    API

---
+++ hoomxb/Network/HxbAPI/LoanBuyConfirmAPI.m
@@ -0,0 +1,13 @@
+//
+//  LoanBuyConfirmAPI.m
+//  hoomxb
+//
+//  Created by HXB-C on 2017/4/18.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "LoanBuyConfirmAPI.h"
+
+@implementation LoanBuyConfirmAPI
+
+@end
