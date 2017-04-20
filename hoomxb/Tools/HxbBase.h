396c20f72034cedcf972b7fb4efd1ad40e7e1401
lipengyue <pengyue.li.@hoomsun.com>
2017-04-20 17:49:51 +0800

    对倒计时工具类进行调整

--- hoomxb/Tools/HxbBase.h
+++ hoomxb/Tools/HxbBase.h
@@ -7,9 +7,17 @@
 //
 
 
-#import "HXBBaseHandDate.h"//关于时间处理的类
+//MARK: UIViewController
 #import "HXBBaseViewController.h"//控制器的基类
+
+//MARK: UIView
 #import "HXBBaseTableView.h"//tableView
 #import "HXBBaseTableViewCell.h"//tableViewCell
 #import "HXBBaseCollectionView.h"//collectionView
 #import "HXBBaseCollectionViewCell.h"//collectionViewCell
+
+
+//MARK: NSObject
+#import "HXBBaseHandDate.h"//关于时间处理的类
+#import "HXBServerAndClientTime.h"//与服务器时间协调
+#import "HXBBaseContDownManager.h"//关于倒计时的类
