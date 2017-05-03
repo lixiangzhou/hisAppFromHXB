7469c7b57f21ab7da436bd21fcb7e26eaf929c31
lipengyue <pengyue.li.@hoomsun.com>
2017-05-03 10:54:01 +0800

    管理

--- hoomxb/Tools/Base/BaseObject/HXBBaseContDownManager.h
+++ hoomxb/Tools/Base/BaseObject/HXBBaseContDownManager.h
@@ -27,24 +27,34 @@ typedef enum : NSUInteger {
 
 /**
  * 请用这个方法（或者对应的init方法）创建对象
- * @parma countDownStartTime 剩多长时间开始倒计时（默认剩余60分钟倒计时）
- * @parma countDownUnit 倒计时单位时间（默认为1妙）
- * @parma modelArray 需要倒计时的model数组
- * @parma modelDateKey model储存到期时间的属性名
- * @parma modelCountDownKey model储存剩余时间的属性名
- * @parma modelDateType model中储存的到期时间是否为剩余时间
+ * @param countDownStartTime 剩多长时间开始倒计时（默认剩余60分钟倒计时）
+ * @param countDownUnit 倒计时单位时间（默认为1妙）
+ * @param modelArray 需要倒计时的model数组
+ * @param modelDateKey model储存到期时间的属性名
+ * @param modelCountDownKey model储存剩余时间的属性名
+ * @param modelDateType model中储存的到期时间是否为剩余时间
  */
-+ (instancetype)countDownManagerWithCountDownStartTime: (long)countDownStartTime andCountDownUnit: (double)countDownUnit andModelArray: (NSArray *)modelArray andModelDateKey: (NSString *)modelDateKey andModelCountDownKey: (NSString *)modelCountDownKey andModelDateType: (PYContDownManagerModelDateType)modelDateType;
++ (instancetype)countDownManagerWithCountDownStartTime: (long)countDownStartTime
+                                      andCountDownUnit: (double)countDownUnit
+                                         andModelArray: (NSArray *)modelArray
+                                       andModelDateKey: (NSString *)modelDateKey
+                                  andModelCountDownKey: (NSString *)modelCountDownKey
+                                      andModelDateType: (PYContDownManagerModelDateType)modelDateType;
 /**
  * 请用这个方法（或者对应的init方法）创建对象
- * @parma countDownStartTime 剩多长时间开始倒计时
- * @parma countDownUnit 倒计时单位时间
- * @parma modelArray 需要倒计时的model数组
- * @parma modelDateKey model储存到期时间的属性名
- * @parma modelCountDownKey model储存剩余时间的属性名
- * @parma modelDateType model中储存的到期时间是否为剩余时间
+ * @param countDownStartTime 剩多长时间开始倒计时
+ * @param countDownUnit 倒计时单位时间
+ * @param modelArray 需要倒计时的model数组
+ * @param modelDateKey model储存到期时间的属性名
+ * @param modelCountDownKey model储存剩余时间的属性名
+ * @param modelDateType model中储存的到期时间是否为剩余时间
  */
-- (instancetype)initWithCountDownStartTime: (long)countDownStartTime andCountDownUnit: (double)countDownUnit andModelArray: (NSArray *)modelArray andModelDateKey: (NSString *)modelDateKey andModelCountDownKey: (NSString *)modelCountDownKey andModelDateType: (PYContDownManagerModelDateType)modelDateType;
+- (instancetype)initWithCountDownStartTime: (long)countDownStartTime
+                          andCountDownUnit: (double)countDownUnit
+                             andModelArray: (NSArray *)modelArray
+                           andModelDateKey: (NSString *)modelDateKey
+                      andModelCountDownKey: (NSString *)modelCountDownKey
+                          andModelDateType: (PYContDownManagerModelDateType)modelDateType;
 
 
 /**
