254c58d72a2c0795322fbad7f34a8646dc9deb00
lipengyue <pengyue.li.@hoomsun.com>
2017-06-09 09:54:59 +0800

    no message

---
+++ hoomxb/Network/HxbAPI/Account/HXBAlterLoginPasswordAPI.m
@@ -0,0 +1,13 @@
+//
+//  HXBAlterLoginPasswordAPI.m
+//  hoomxb
+//
+//  Created by HXB on 2017/6/8.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBAlterLoginPasswordAPI.h"
+
+@implementation HXBAlterLoginPasswordAPI
+
+@end
