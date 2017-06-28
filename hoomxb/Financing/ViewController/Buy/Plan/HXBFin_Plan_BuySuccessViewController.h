0ee6aded6309ff7a422060c8bd37ee2a6c588974
lipengyue <pengyue.li.@hoomsun.com>
2017-06-28 10:38:02 +0800

    购买成功

---
+++ hoomxb/Financing/ViewController/Buy/Plan/HXBFin_Plan_BuySuccessViewController.h
@@ -0,0 +1,14 @@
+//
+//  HXBFin_Plan_BuySuccessViewController.h
+//  hoomxb
+//
+//  Created by HXB on 2017/6/27.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBBaseViewController.h"
+@class HXBFin_Plan_BuyViewModel;
+///购买成功页
+@interface HXBFin_Plan_BuySuccessViewController : HXBBaseViewController
+@property (nonatomic,strong) HXBFin_Plan_BuyViewModel *planModel;
+@end
