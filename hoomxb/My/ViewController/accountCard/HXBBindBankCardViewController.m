b7e9e17f1b558a5a68c283cd4a8cb0fea8e5ed62
刘鹏 <peng.liu@hoomsun.com>
2017-07-07 16:20:57 +0800

    绑卡获取验证码

--- hoomxb/My/ViewController/accountCard/HXBBindBankCardViewController.m
+++ hoomxb/My/ViewController/accountCard/HXBBindBankCardViewController.m
@@ -7,23 +7,40 @@
 //
 
 #import "HXBBindBankCardViewController.h"
-
+#import "HXBCardholderInformationView.h"
 @interface HXBBindBankCardViewController ()
 
+@property (nonatomic, strong) HXBCardholderInformationView *cardholderInformationView;
 @end
 
 @implementation HXBBindBankCardViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.view addSubview:self.cardholderInformationView];
+    
+    [self setSubViewFrame];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setSubViewFrame
+{
+    [self.cardholderInformationView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view.mas_left);
+        make.right.equalTo(self.view.mas_right);
+        make.top.equalTo(self.view.mas_top).offset(64);
+        make.height.equalTo(@kScrAdaptationH(80));
+    }];
 }
 
 
+#pragma mark - 懒加载
+- (HXBCardholderInformationView *)cardholderInformationView
+{
+    if (!_cardholderInformationView) {
+        _cardholderInformationView = [[HXBCardholderInformationView alloc] init];
+    }
+    return _cardholderInformationView;
+}
 
 @end
