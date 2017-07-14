fc844ff68b39d50bb504a96d8c7063c7602988d2
刘鹏 <peng.liu@hoomsun.com>
2017-07-14 10:26:39 +0800

    更新分支代码

--- hoomxb/Home/View/HXBHomePageBulletinView.m
+++ hoomxb/Home/View/HXBHomePageBulletinView.m
@@ -243,22 +243,22 @@
 //    return _contentScrollView;
 //}
 #pragma mark - 重写父类的方法
--  (void)drawRect:(CGRect)rect
-{
-    [super drawRect:rect];
-    CGContextRef currentContext = UIGraphicsGetCurrentContext();
-    //设置虚线颜色
-    CGContextSetStrokeColorWithColor(currentContext, [UIColor blackColor].CGColor);
-    //设置虚线宽度
-    CGContextSetLineWidth(currentContext, 1);
-    //设置虚线绘制起点
-    CGContextMoveToPoint(currentContext, self.recommendedTitleLabel.x, self.height - 1);
-    //设置虚线绘制终点
-    CGContextAddLineToPoint(currentContext, self.width - self.x, self.height - 1);
-    //设置虚线排列的宽度间隔:下面的arr中的数字表示先绘制3个点再绘制1个点
-    CGFloat arr[] = {3,1};
-    //下面最后一个参数“2”代表排列的个数。
-    CGContextSetLineDash(currentContext, 0, arr, 2);
-    CGContextDrawPath(currentContext, kCGPathStroke);
-}
+//-  (void)drawRect:(CGRect)rect
+//{
+//    [super drawRect:rect];
+//    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+//    //设置虚线颜色
+//    CGContextSetStrokeColorWithColor(currentContext, [UIColor blackColor].CGColor);
+//    //设置虚线宽度
+//    CGContextSetLineWidth(currentContext, 1);
+//    //设置虚线绘制起点
+//    CGContextMoveToPoint(currentContext, self.recommendedTitleLabel.x, self.height - 1);
+//    //设置虚线绘制终点
+//    CGContextAddLineToPoint(currentContext, self.width - self.x, self.height - 1);
+//    //设置虚线排列的宽度间隔:下面的arr中的数字表示先绘制3个点再绘制1个点
+//    CGFloat arr[] = {3,1};
+//    //下面最后一个参数“2”代表排列的个数。
+//    CGContextSetLineDash(currentContext, 0, arr, 2);
+//    CGContextDrawPath(currentContext, kCGPathStroke);
+//}
 @end
