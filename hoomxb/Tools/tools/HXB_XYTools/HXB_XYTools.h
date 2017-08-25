35fc33e1021990b9c8b2c369cbd4962832f28b85
刘鹏 <peng.liu@hoomsun.com>
2017-08-25 10:24:12 +0800

    合并代码

---
+++ hoomxb/Tools/tools/HXB_XYTools/HXB_XYTools.h
@@ -0,0 +1,16 @@
+//
+//  HXB_XYTools.h
+//  hoomxb
+//
+//  Created by HXB on 2017/8/24.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXB_XYTools : NSObject
+
+
+-(UIImage*)convertViewToImage:(UIView*)view;
+
+@end
---
+++ hoomxb/Tools/tools/HXB_XYTools/HXB_XYTools.m
@@ -0,0 +1,25 @@
+//
+//  HXB_XYTools.m
+//  hoomxb
+//
+//  Created by HXB on 2017/8/24.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXB_XYTools.h"
+
+@implementation HXB_XYTools
+
+-(UIImage*)convertViewToImage:(UIView*)view {
+    CGSize size = view.bounds.size;
+    // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了
+    UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage*image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+
+
+@end
