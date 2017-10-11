c5deea93c940dc8cb92d31f33a06751bdd3a1f2a
baiyingchun <baiyingchun@hoomsun.com>
2017-10-11 11:42:49 +0800

    删除标的等级（p1需求），为了合并到develop_base分支

--- hoomxb/Financing/View/Loan/BorrowingInformation/HXBLoanInstructionView.m
+++ hoomxb/Financing/View/Loan/BorrowingInformation/HXBLoanInstructionView.m
@@ -14,11 +14,6 @@
 
 @property (nonatomic, strong) UILabel *loanContentLabel;
 
-@property (nonatomic, strong) UILabel       *safetyLevelInstructionLabel;//安全等级
-@property (nonatomic, strong) UIImageView   *safetyLevelIconImageView;
-@property (nonatomic, strong) UILabel       *safetyLevelLabel;
-@property (nonatomic, strong) UILabel       *safetyLevelContentContentLabel;
-
 @end
 
 @implementation HXBLoanInstructionView
@@ -29,10 +24,7 @@
         self.backgroundColor = [UIColor whiteColor];
         [self addSubview:self.loanInstructionLabel];
         [self addSubview:self.loanContentLabel];
-        [self addSubview:self.safetyLevelInstructionLabel];
-        [self addSubview:self.safetyLevelIconImageView];
-        [self addSubview:self.safetyLevelLabel];
-        [self addSubview:self.safetyLevelContentContentLabel];
+        
         [self setupSubViewframe];
     }
     return self;
@@ -52,8 +44,7 @@
 {
     [self mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.loanInstructionLabel.mas_top).offset(kScrAdaptationH(-15));
-//        make.bottom.equalTo(self.loanContentLabel.mas_bottom).offset(kScrAdaptationH(15));
-        make.bottom.equalTo(self.safetyLevelContentContentLabel.mas_bottom).offset(kScrAdaptationH(15));
+        make.bottom.equalTo(self.loanContentLabel.mas_bottom).offset(kScrAdaptationH(15));
     }];
     [self.loanInstructionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(@(kScrAdaptationH(15)));
@@ -63,28 +54,6 @@
         make.top.equalTo(self.loanInstructionLabel.mas_bottom).offset(kScrAdaptationH(15));
         make.left.equalTo(@(kScrAdaptationW(15)));
         make.right.equalTo(self.mas_right).offset(kScrAdaptationW(-15));
-    }];
-    
-    [self.safetyLevelInstructionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.loanContentLabel.mas_bottom).offset(kScrAdaptationH(15));
-        make.left.equalTo(@(kScrAdaptationW(15)));
-    }];
-    [self.safetyLevelIconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(@(kScrAdaptationW(15)));
-        make.top.equalTo(self.safetyLevelLabel).offset(kScrAdaptationH(3.5));
-        make.height.equalTo(@(kScrAdaptationH(10.3)));
-        make.width.equalTo(@(kScrAdaptationW(8.5)));
-    }];
-    [self.safetyLevelLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.safetyLevelInstructionLabel.mas_bottom).offset(kScrAdaptationH(15));
-        make.left.equalTo(self.safetyLevelIconImageView.mas_right).offset(kScrAdaptationH(4));
-        make.height.equalTo(@(kScrAdaptationW(17)));
-        make.width.equalTo(@(kScrAdaptationW(20)));
-    }];
-    [self.safetyLevelContentContentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.safetyLevelInstructionLabel.mas_bottom).offset(kScrAdaptationH(15));
-        make.left.equalTo(self.safetyLevelLabel.mas_right);
-        make.right.equalTo(self.mas_right).offset(kScrAdaptationW(-70));
     }];
 }
 
@@ -117,48 +86,6 @@
         _loanContentLabel.text = @"借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明借款说明";
     }
     return _loanContentLabel;
-}
-
-- (UILabel *)safetyLevelInstructionLabel
-{
-    if (!_safetyLevelInstructionLabel) {
-        _safetyLevelInstructionLabel = [[UILabel alloc] init];
-        _safetyLevelInstructionLabel.font = kHXBFont_PINGFANGSC_REGULAR(15);
-        _safetyLevelInstructionLabel.textColor = kHXBColor_Grey_Font0_2;
-        _safetyLevelInstructionLabel.text = @"安全等级";
-    }
-    return _safetyLevelInstructionLabel;
-}
-
-- (UILabel *)safetyLevelContentContentLabel
-{
-    if (!_safetyLevelContentContentLabel) {
-        _safetyLevelContentContentLabel = [[UILabel alloc] init];
-        _safetyLevelContentContentLabel.font = kHXBFont_PINGFANGSC_REGULAR(14);
-        _safetyLevelContentContentLabel.textColor = kHXBColor_Font0_6;
-        _safetyLevelContentContentLabel.numberOfLines = 0;
-        _safetyLevelContentContentLabel.text = @"风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明风险等级说明";
-    }
-    return _safetyLevelContentContentLabel;
-}
-
-- (UILabel *)safetyLevelLabel{
-    if (!_safetyLevelLabel) {
-        _safetyLevelLabel = [[UILabel alloc] init];
-        _safetyLevelLabel.font = kHXBFont_PINGFANGSC_REGULAR(14);
-        _safetyLevelLabel.textColor = RGB(255, 163, 56);
-        _safetyLevelLabel.numberOfLines = 1;
-        _safetyLevelLabel.text = @"AA";
-    }
-    return _safetyLevelLabel;
-}
-
-- (UIImageView *)safetyLevelIconImageView{
-    if (!_safetyLevelIconImageView) {
-        _safetyLevelIconImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
-        _safetyLevelIconImageView.image = [UIImage imageNamed:@"security_ level"];
-    }
-    return _safetyLevelIconImageView;
 }
 
 @end
