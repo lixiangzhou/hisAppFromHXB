eab5d9b4844ca826892cb942a748e5f3ac01f152
李向洲 <lixiangzhouno1@163.com>
2017-11-23 14:39:10 +0800

    对新特性界面优化及代码调整

--- hoomxb/NewFeature/Controller/AXHNewFeatureController.m
+++ hoomxb/NewFeature/Controller/AXHNewFeatureController.m
@@ -15,118 +15,80 @@
 #import "HXBRootVCManager.h"
 
 @interface AXHNewFeatureController ()<TAPageControlDelegate>
-@property (strong, nonatomic) TAPageControl *customPageControl2;
+@property (strong, nonatomic) TAPageControl *pageControl;
 @property (nonatomic, strong) NSArray *imageData;
-@property (nonatomic, strong) AXHNewFeatureCell *cell;
-
 @property (nonatomic, strong) UIButton *startButton;
 
 @end
 
 @implementation AXHNewFeatureController
-static NSString *ID = @"collectionCell";
-//流水布局必须设置一种布局
+
 - (instancetype)init
 {
     UICollectionViewFlowLayout *layout =[[UICollectionViewFlowLayout alloc] init];
-    //设置cell的大小
     layout.itemSize = [UIScreen mainScreen].bounds.size;
-    //清空行距
     layout.minimumLineSpacing = 0;
-    //设置滚动方向
     layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
     return [super initWithCollectionViewLayout:layout];
 }
-//self.view != self.collectionView
-//注意self.collectionView是self.view 的子控件
-//1.使用UICollectionViewController
-//初始化的时候设置布局参数
-//必须UICollectionView要注册cell
-//自定义cell
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.imageData =  @[@"HXBBankCustody", @"HXBSecurityGuarantee", @"HXBBonusPlan", @"HXBCoupon"];
+    
+    [self setUI];
+}
+
 - (void)viewWillAppear:(BOOL)animated{
     self.navigationController.navigationBarHidden = YES;
 }
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.imageData =  @[@"HXBBankCustody", @"HXBSecurityGuarantee", @"HXBBonusPlan", @"HXBCoupon"];
-    
-//    self.collectionView.backgroundColor = [UIColor greenColor];
-    //注册一个cell,默认就会创建这个类型的cell
-    [self.collectionView registerClass:[AXHNewFeatureCell class] forCellWithReuseIdentifier:ID];
-    //分页效果
+
+- (void)setUI
+{
+    [self.collectionView registerClass:[AXHNewFeatureCell class] forCellWithReuseIdentifier:AXHNewFeatureCellID];
     self.collectionView.pagingEnabled = YES;
     self.collectionView.bounces = NO;
     self.collectionView.showsHorizontalScrollIndicator = NO;
-    // 添加pageController
-    [self setUpPageControl];
-
-}
-// 添加pageController
-- (void)setUpPageControl
-{
-    // 添加pageController,只需要设置位置，不需要管理尺寸
     
-    self.customPageControl2               = [[TAPageControl alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame) - kScrAdaptationH750(120), CGRectGetWidth(self.view.frame), 40)];
-    // Example for touch bullet event
-    self.customPageControl2.delegate      = self;
-    self.customPageControl2.numberOfPages = self.imageData.count;
-    // Custom dot view
-    self.customPageControl2.dotViewClass  = [TAExampleDotView class];
-    self.customPageControl2.dotSize       = CGSizeMake(kScrAdaptationH750(14), kScrAdaptationH750(14));
-    [self.view addSubview:self.customPageControl2];
+    self.pageControl = [[TAPageControl alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame) - kScrAdaptationH750(120), CGRectGetWidth(self.view.frame), 40)];
+    self.pageControl.delegate = self;
+    self.pageControl.numberOfPages = self.imageData.count;
+    self.pageControl.dotViewClass = [TAExampleDotView class];
+    self.pageControl.dotSize = CGSizeMake(kScrAdaptationH750(14), kScrAdaptationH750(14));
+    [self.view addSubview:self.pageControl];
+    
     [self.view addSubview:self.startButton];
     [self.startButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.offset(kScrAdaptationH750(350));
         make.height.offset(kScrAdaptationH750(82));
         make.centerX.equalTo(self.view);
-        make.bottom.equalTo(self.customPageControl2.mas_top).offset(-kScrAdaptationH750(100));
+        make.bottom.equalTo(self.pageControl.mas_top).offset(-kScrAdaptationH750(100));
     }];
 }
 
-// 只要一滚动就会调用
+#pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    // 获取当前的偏移量，计算当前第几页
     int page = scrollView.contentOffset.x / scrollView.bounds.size.width + 0.5;
-    
-    // 设置页数
-    self.customPageControl2.currentPage = page;
-    if (page == self.imageData.count - 1) { // 最后一页,显示分享和开始按钮
-        self.startButton.hidden = NO;
-    }else{ // 非最后一页，隐藏分享和开始按钮
-        self.startButton.hidden = YES;
-    }
+    self.pageControl.currentPage = page;
+    self.startButton.hidden = page != self.imageData.count - 1;
 }
 
-//返回多少组
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
-{
-    return 1;
-}
-
-//返回第section组有多少个cell
+#pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
     return self.imageData.count;
 }
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    
-    // dequeueReusableCellWithReuseIdentifier
-    //1.首先从缓存池里面取cell
-    //2.看当前是否有注册过cell，如果注册了cell，就会帮你创建cell
-    //3.没有注册就会报错
-    AXHNewFeatureCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID forIndexPath:indexPath];
-    _cell = cell;
-     // 拼接图片名称 3.5 320 480
-//    CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
+    AXHNewFeatureCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:AXHNewFeatureCellID forIndexPath:indexPath];
     NSString *imageName = self.imageData[indexPath.row];
- 
     cell.image = [UIImage imageNamed:imageName];
-    
     return cell;
 }
 
+#pragma mark - Lazy
 - (UIButton *)startButton
 {
     if (_startButton == nil) {
@@ -144,7 +106,8 @@ static NSString *ID = @"collectionCell";
     }
     return _startButton;
 }
-// 点击开始微博的时候调用
+
+#pragma mark - Action
 - (void)start
 {
     [[HXBRootVCManager manager] enterTheGesturePasswordVCOrTabBar];
@@ -162,10 +125,8 @@ static NSString *ID = @"collectionCell";
     alertVC.immediateOpenBlock = ^{
         [HXBUmengManagar HXB_clickEventWithEnevtId:kHXBUmeng_alertBtn];
         HXBOpenDepositAccountViewController *openDepositAccountVC = [[HXBOpenDepositAccountViewController alloc] init];
-//        openDepositAccountVC.userModel = viewModel;
         openDepositAccountVC.title = @"开通存管账户";
         openDepositAccountVC.type = HXBRechargeAndWithdrawalsLogicalJudgment_Other;
-//        KeyWindow.rootViewController = self.mainTabbarVC;
         [[HXBRootVCManager manager].mainTabbarVC.childViewControllers[0] pushViewController:openDepositAccountVC animated:YES];
     };
     [KeyWindow.rootViewController presentViewController:alertVC animated:NO completion:nil];
