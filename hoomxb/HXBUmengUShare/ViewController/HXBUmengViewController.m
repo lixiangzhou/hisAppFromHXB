34cc1fc0214422c1dfec050891155a9693507b02
翟聪聪 <congcong.zhai@hoomsun.com>
2017-12-11 20:20:08 +0800

    修改了分享界面逻辑

--- hoomxb/HXBUmengUShare/ViewController/HXBUmengViewController.m
+++ hoomxb/HXBUmengUShare/ViewController/HXBUmengViewController.m
@@ -112,6 +112,7 @@
 {
     if (!KeyChain.ishaveNet) {
          [HxbHUDProgress showMessageCenter:kNoNetworkText inView:nil];
+         [self cancelShareView];
         return;
     }
     if (!self.shareVM.shareModel) {
