d21cf8bf67b7e4174e3d225c551e4325249b89e6
caihongji <cai_hongji@sina.cn>
2017-12-14 14:18:03 +0800

    主题适配管理类

---
+++ hoomxb/Tools/tools/Manager/HXBThemAdapterManager.h
@@ -0,0 +1,21 @@
+//
+//  HXBThemAdapterManager.h
+//  hoomxb
+//
+//  Created by caihongji on 2017/12/14.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXBThemAdapterManager : NSObject
+//font 适配相关
+
+/**
+ 获取UIFont
+
+ @param fontSize 文字大小
+ @return 适配后的字体大小
+ */
++ (CGFloat)getAdaterFont:(CGFloat)fontSize;
+@end
---
+++ hoomxb/Tools/tools/Manager/HXBThemAdapterManager.m
@@ -0,0 +1,23 @@
+//
+//  HXBThemAdapterManager.m
+//  hoomxb
+//
+//  Created by caihongji on 2017/12/14.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBThemAdapterManager.h"
+
+@implementation HXBThemAdapterManager
+
+/**
+ 获取UIFont
+ 
+ @param fontSize 文字大小
+ @return 适配后的字体
+ */
++ (CGFloat)getAdaterFont:(CGFloat)fontSize
+{
+    
+}
+@end
