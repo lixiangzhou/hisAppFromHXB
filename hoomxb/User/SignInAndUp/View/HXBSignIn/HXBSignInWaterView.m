41f185f2b780e4006702829c8f7dc3c52f7284fd
李向洲 <lixiangzhouno1@163.com>
2017-12-15 14:09:43 +0800

    我的页面 + 字体适配，size 适配

--- hoomxb/User/SignInAndUp/View/HXBSignIn/HXBSignInWaterView.m
+++ hoomxb/User/SignInAndUp/View/HXBSignIn/HXBSignInWaterView.m
@@ -25,16 +25,16 @@
     return self;
 }
 - (void)setUP {
-    CGFloat adaptXHeight = HXBIPhoneX ? 24 : 0;
-    HXBNavWaterView *wave1 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 120 + adaptXHeight)];
+    
+    HXBNavWaterView *wave1 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 120 + HXBStatusBarAdditionHeight)];
     wave1.alpha = 0.65;
     [self addSubview:wave1];
     
-    HXBNavWaterView *wave2 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 105 + adaptXHeight)];
+    HXBNavWaterView *wave2 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 105 + HXBStatusBarAdditionHeight)];
     [self addSubview:wave2];
     wave2.alpha = 0.75;
     
-    HXBNavWaterView *wave3 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 90 + adaptXHeight)];
+    HXBNavWaterView *wave3 = [[HXBNavWaterView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 90 + HXBStatusBarAdditionHeight)];
     [self addSubview:wave3];
     wave3.alpha = 0.85;
 }
