7b03516dfe902961b8ebeb0d02d0f327eb566099
白迎春 <yingchun.bai@hoomsun.com>
2018-01-10 14:03:50 +0800

    按月计息账户外计划详情改变图标点击区域

--- hoomxb/Financing/View/Plan/PlanDetail_Detail/HXBFinPlanDetail_DetailView.m
+++ hoomxb/Financing/View/Plan/PlanDetail_Detail/HXBFinPlanDetail_DetailView.m
@@ -100,16 +100,30 @@
     UILabel * rightView = (UILabel *)self.typeView.rightViewArray[1];
     if ([self.typeView.cashType isEqualToString:FIN_PLAN_INCOMEAPPROACH_MONTHLY] && rightView.subviews.count > 1) {
         UIButton *infoBtn = nil;
+        UILabel *lab = nil;
         for (UIView *view in rightView.subviews) {
             if ([view isKindOfClass:[UIButton class]]) {
                 infoBtn = (UIButton *)view;
             }
+            if ([view isKindOfClass:[UILabel class]]) {
+                lab = (UILabel *)view;
+            }
         }
         [infoBtn setImage:[UIImage imageNamed:@"lightblue_tip"] forState:UIControlStateNormal];
-        rightView.userInteractionEnabled = YES;
         [infoBtn addTarget:self action:@selector(clickInfoBtn) forControlEvents:UIControlEventTouchUpInside];
+        
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickInfoBtn)];
+        
+        rightView.userInteractionEnabled = YES;
+        [rightView addGestureRecognizer:tap];
+        
+        lab.userInteractionEnabled = YES;
+        [lab addGestureRecognizer:tap];
+        
+        UILabel * leftView = (UILabel *)self.typeView.leftViewArray[1];
+        leftView.userInteractionEnabled = YES;
+        [leftView addGestureRecognizer:tap];
     }
-    
     
     self.serverView =  [[HXBBaseView_MoreTopBottomView alloc]initWithFrame:CGRectZero andTopBottomViewNumber:1 andViewClass:[UILabel class] andViewHeight:kScrAdaptationH(15) andTopBottomSpace:kScrAdaptationH(20) andLeftRightLeftProportion:1.0/3 Space:edgeInsets andCashType:nil];
     UILabel *button = (UILabel *)self.serverView.rightViewArray.firstObject;
