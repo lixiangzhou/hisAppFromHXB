ecf3a6f9b5332452cc46c4ca16788db936b93c34
caihongji <cai_hongji@sina.cn>
2018-01-16 17:51:14 +0800

    重置webview基类的代码

--- hoomxb/Base/BaseController/WebViewController/HXBBaseWKWebViewController.m
+++ hoomxb/Base/BaseController/WebViewController/HXBBaseWKWebViewController.m
@@ -110,7 +110,6 @@
     [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.bottom.equalTo(weakSelf.view);
         make.top.equalTo(weakSelf.progressView.mas_bottom);
-        make.bottom.equalTo(weakSelf.view);
     }];
 }
 
