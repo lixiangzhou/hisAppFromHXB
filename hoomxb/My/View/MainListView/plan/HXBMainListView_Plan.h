d681f211ecf140ec030485fde6aea1431d12b94c
caihongji <cai_hongji@sina.cn>
2018-01-30 14:49:12 +0800

    http://jira.hoomxb.com/browse/HXB-1096， 代码优化

--- hoomxb/My/View/MainListView/plan/HXBMainListView_Plan.h
+++ hoomxb/My/View/MainListView/plan/HXBMainListView_Plan.h
@@ -26,6 +26,14 @@
 ///plan 已退出
 @property (nonatomic,strong) NSArray <HXBMYViewModel_MianPlanViewModel *>*exit_Plan_array;
 
+@property (nonatomic, assign) BOOL isHoldPlanLastPage; //持有中是否最后一页
+@property (nonatomic, assign) BOOL isHoldPlanShowLoadMore; //持有中是否显示加载更多
+
+@property (nonatomic, assign) BOOL isExitingLastPage; //退出中是否最后一页
+@property (nonatomic, assign) BOOL isExitingShowLoadMore; //退出中是否显示加载更多
+
+@property (nonatomic, assign) BOOL isExitedLastPage; //已退出是否最后一页
+@property (nonatomic, assign) BOOL isExitedShowLoadMore; //退出中是否显示加载更多
 
 #pragma mark - 刷新
 ///资产统计的刷新
