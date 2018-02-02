a2c977efc600634a5012fff8c002969f311c0f2b
caihongji <cai_hongji@sina.cn>
2018-02-02 11:37:53 +0800

    清理分支

---
+++ hoomxb/Home/Home/View/HxbHomePageFreshCustomerView.h
@@ -0,0 +1,13 @@
+//
+//  HxbHomePageFreshCustomerView.h
+//  hoomxb
+//
+//  Created by HXB-C on 2017/5/11.
+//  Copyright © 2017年 hoomsun-miniX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HxbHomePageFreshCustomerView : UIView
+
+@end
