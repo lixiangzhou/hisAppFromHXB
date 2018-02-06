6a15da59ff1c504c543e6f2f09fa427393e95b5f
caihongji <cai_hongji@sina.cn>
2018-02-06 15:37:41 +0800

    清理冗余文件

--- hoomxb/My/Request/HXBMYRequest.h
+++ hoomxb/My/Request/HXBMYRequest.h
@@ -31,31 +31,6 @@
 @property (nonatomic, assign) BOOL isShowHUD;
 
 #pragma mark - 主要页面的网络请求
-///资金统计的Request plan
-- (void)myPlanAssetStatistics_requestWithSuccessBlock: (void(^)(HXBMYModel_AssetStatistics_Plan *model))successDateBlock andFailureBlock: (void(^)(NSError *error))failureBlock;
-
-///红利计划 列表的网络数据请求
-- (void)myPlan_requestWithPlanType: (HXBRequestType_MY_PlanRequestType)planRequestType
-                         andUpData: (BOOL)isUPData
-                   andSuccessBlock: (void(^)(NSArray<HXBMYViewModel_MianPlanViewModel *>* viewModelArray, NSInteger totalCount))successDateBlock
-                   andFailureBlock: (void(^)(NSError *error))failureBlock;
-
-
-///资金统计的Request loan
-- (void)myLoanAssetStatistics_requestWithSuccessBlock: (void(^)(NSArray <HXBMYModel_AssetStatistics_Loan *>*model))successDateBlock andFailureBlock: (void(^)(NSError *error))failureBlock;
-
-
-///散标 列表的网络数据请求
-- (void)myLoan_requestWithLoanType: (HXBRequestType_MY_LoanRequestType)LoanRequestType
-                         andUpData: (BOOL)isUPData
-                   andSuccessBlock: (void(^)(NSArray<HXBMYViewModel_MainLoanViewModel *>* viewModelArray, NSInteger totalCount))successDateBlock
-                   andFailureBlock: (void(^)(NSError *error))failureBlock;
-
-///转让中 列表的网络数据的请求
-- (void)myLoanTruansfer_requestWithLoanTruansferWithIsUPData: (BOOL)isUPData
-                                             andSuccessBlock: (void(^)(NSArray<HXBMY_LoanTruansferViewModel *>* viewModelArray))successDateBlock
-                                             andFailureBlock: (void(^)(NSError *error,HXBBaseRequest *request))failureBlock;
-
 
 /// 资金记录 接口
 - (void)capitalRecord_requestWithScreenType: (NSString *)screenType
@@ -73,17 +48,6 @@
                         andSuccessBlock: (void(^)(NSArray<HXBMY_PlanViewModel_LoanRecordViewModel *>* viewModelArray))successDateBlock
                         andFailureBlock: (void(^)(NSError *error))failureBlock;
 
-/**
- loan  账户内散标资产
- */
-- (void)loanAssets_AccountRequestSuccessBlock: (void(^)(HXBMYModel_Loan_LoanRequestModel *viewModel))successDateBlock
-                              andFailureBlock: (void(^)(NSError *error))failureBlock;
-/**
- plan 账户内计划资产
- */
-- (void)planAssets_AccountRequestSuccessBlock: (void(^)(HXBMYModel_Plan_planRequestModel *viewModel))successDateBlock
-                              andFailureBlock: (void(^)(NSError *error))failureBlock
-                                    andUpData: (BOOL)isUPData;
 /**
  账户内-债权转让确认页
  */
