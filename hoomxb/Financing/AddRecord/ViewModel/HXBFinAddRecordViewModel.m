b2150bf13976985a812da314f305bb23da4b194f
白迎春 <yingchun.bai@hoomsun.com>
2018-02-25 17:10:06 +0800

    删除多余注释

--- hoomxb/Financing/AddRecord/ViewModel/HXBFinAddRecordViewModel.m
+++ hoomxb/Financing/AddRecord/ViewModel/HXBFinAddRecordViewModel.m
@@ -26,24 +26,6 @@
         };
     } failure:^(NYBaseRequest *request, NSError *error) {
     }];
-    
-    
-//    HXBBaseRequest *planAddRecortdAPI = [[HXBBaseRequest alloc]initWithDelegate:self];
-//    planAddRecortdAPI.isUPReloadData = isUPLoad;
-//    planAddRecortdAPI.requestMethod = NYRequestMethodGet;
-//    planAddRecortdAPI.requestUrl = kHXBFinanc_Plan_AddRecortdURL(planId);
-//
-//    [planAddRecortdAPI loadDataWithSuccess:^(HXBBaseRequest *request, id responseObject) {
-//
-//        HXBFinModel_AddRecortdModel_Plan *planAddRecortdModel = [[HXBFinModel_AddRecortdModel_Plan alloc]init];
-//        NSDictionary *dataDic = [responseObject valueForKey:@"data"];
-//        [planAddRecortdModel yy_modelSetWithDictionary:dataDic];
-//        weakSelf.addRecortdModel_PlanModel = planAddRecortdModel;
-//        if (resultBlock) {
-//            resultBlock(YES,nil);
-//        };
-//    } failure:^(HXBBaseRequest *request, NSError *error) {
-//    }];
 }
 
 - (void)requestLoanAddRecortdWithId:(NSString *)loanId loadAddRecortdWithISUPLoad:(BOOL)isUPLoad andOrder: (NSString *)order resultBlock:(void (^)(BOOL, NSError *))resultBlock{
@@ -61,20 +43,6 @@
         }
     } failure:^(NYBaseRequest *request, NSError *error) {
     }];
-    
-//    HXBBaseRequest *loanAddRecortdAPI = [[HXBBaseRequest alloc]initWithDelegate:self];
-//    loanAddRecortdAPI.requestUrl = kHXBFinanc_Loan_AddRecortdURL(loanId);
-//    [loanAddRecortdAPI loadDataWithSuccess:^(HXBBaseRequest *request, id responseObject) {
-//
-//        FinModel_AddRecortdModel_Loan *model = [[FinModel_AddRecortdModel_Loan alloc]init];
-//        NSDictionary *dic = [responseObject valueForKey:@"data"];
-//        [model yy_modelSetWithDictionary:dic];
-//        weakSelf.addRecortdModel_LoanModel = model;
-//        if (resultBlock) {
-//            resultBlock(YES,nil);
-//        }
-//    } failure:^(HXBBaseRequest *request, NSError *error) {
-//    }];
 }
 
 - (void)requestLoanTruaLnsferAddRecortdWithId:(NSString *)loanTruaLnsferId loanTruansferAddRecortdWithISUPLoad: (BOOL)isUPLoad andOrder: (NSString *)order resultBlock:(void (^)(BOOL, NSError *))resultBlock{
@@ -99,33 +67,6 @@
         }
     } failure:^(NYBaseRequest *request, NSError *error) {
     }];
-    
-//    HXBBaseRequest *loanTruansferAddRecortdAPI = [[HXBBaseRequest alloc]initWithDelegate:self];
-//    loanTruansferAddRecortdAPI.requestMethod = NYRequestMethodGet;
-//    loanTruansferAddRecortdAPI.isUPReloadData = isUPLoad;
-//    loanTruansferAddRecortdAPI.requestArgument = @{
-//                                                   @"page":@(loanTruansferAddRecortdAPI.dataPage)
-//                                                   };
-//    loanTruansferAddRecortdAPI.requestUrl = kHXBFinanc_LoanTruansfer_AddRecortdURL(loanTruaLnsferId);
-//
-//    [loanTruansferAddRecortdAPI loadDataWithSuccess:^(HXBBaseRequest *request, id responseObject) {
-//
-//        NSArray *array = responseObject[kResponseData][kResponseDataList];
-//        NSMutableArray *dataArray = [[NSMutableArray alloc]init];
-//
-//        [array enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//            HXBFinModel_AddRecortdModel_LoanTruansfer *model = [[HXBFinModel_AddRecortdModel_LoanTruansfer alloc]init];
-//            [model yy_modelSetWithDictionary:obj];
-//            model.index = @(array.count - idx).description;
-//            [dataArray addObject:model];
-//        }];
-//        weakSelf.loanTruansferRecortdModelArray = dataArray;
-//        if (resultBlock) {
-//            resultBlock(YES,nil);
-//        }
-//    } failure:^(HXBBaseRequest *request, NSError *error) {
-//
-//    }];
 }
 
 @end
