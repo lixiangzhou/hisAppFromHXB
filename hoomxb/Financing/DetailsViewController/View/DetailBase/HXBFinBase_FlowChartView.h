38a037b74d0e1bbebd786c0846e050bd717f495c
caihongji <cai_hongji@sina.cn>
2018-03-14 18:43:17 +0800

    修改文案

--- hoomxb/Financing/DetailsViewController/View/DetailBase/HXBFinBase_FlowChartView.h
+++ hoomxb/Financing/DetailsViewController/View/DetailBase/HXBFinBase_FlowChartView.h
@@ -21,7 +21,7 @@ typedef enum : NSUInteger {
 
 static NSString *kHXB_FinPlan_Add = @"加入";
 static NSString *kHXB_FinPlan_Begin = @"进入锁定期";
-static NSString *kHXB_FinPlan_Leave = @"到期退出";
+static NSString *kHXB_FinPlan_Leave = @"到期可申请退出";
 @class HXBFinBase_FlowChartView_Manager;
 @interface HXBFinBase_FlowChartView : UIView
 /**
