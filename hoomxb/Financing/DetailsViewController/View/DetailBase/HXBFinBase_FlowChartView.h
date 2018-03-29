2da82779c7289e8f319837ba705c2c56a39e5f45
caihongji <cai_hongji@sina.cn>
2018-03-29 18:46:34 +0800

    账户外计划详情页的进度条样式修改

--- hoomxb/Financing/DetailsViewController/View/DetailBase/HXBFinBase_FlowChartView.h
+++ hoomxb/Financing/DetailsViewController/View/DetailBase/HXBFinBase_FlowChartView.h
@@ -19,23 +19,8 @@ typedef enum : NSUInteger {
     HXBFinBase_FlowChartView_Plan_Stage_Leave,
 } HXBFinBase_FlowChartView_Plan_Stage;
 
-static NSString *kHXB_FinPlan_Add = @"加入";
-static NSString *kHXB_FinPlan_Begin = @"进入锁定期";
-static NSString *kHXB_FinPlan_Leave = @"到期可申请退出";
 @class HXBFinBase_FlowChartView_Manager;
 @interface HXBFinBase_FlowChartView : UIView
-/**
- 第几个阶段
- 加入中，开始收益，到期退出
- */
-@property (nonatomic,assign) NSInteger stage;
-
-/**
- profitLabel描述
- 原利息复投方式显示：收益复投
- 利息返还方式显示：每月X日付息
- */
-@property (nonatomic,copy) NSString *profitStr;
 
 ///设置值
 - (void)setUPFlowChartViewManagerWithManager: (HXBFinBase_FlowChartView_Manager *(^)(HXBFinBase_FlowChartView_Manager *manager))flowChartViewManagerBlock;
@@ -60,4 +45,10 @@ static NSString *kHXB_FinPlan_Leave = @"到期可申请退出";
  到期退出时间
  */
 @property (nonatomic,copy) NSString * leaveTime;
+/**
+ profitLabel描述
+ 原利息复投方式显示：收益复投
+ 利息返还方式显示：每月X日付息
+ */
+@property (nonatomic,copy) NSString *profitStr;
 @end
