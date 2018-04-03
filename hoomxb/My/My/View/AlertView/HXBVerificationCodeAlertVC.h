243a82f810a1a1c3129b81f2130d9778a9b8968d
白迎春 <953152868@qq.com>
2018-04-03 10:28:40 +0800

    更改文案显示

--- hoomxb/My/My/View/AlertView/HXBVerificationCodeAlertVC.h
+++ hoomxb/My/My/View/AlertView/HXBVerificationCodeAlertVC.h
@@ -20,6 +20,9 @@
  */
 @property (nonatomic, copy) NSString *subTitle;
 
+@property (nonatomic, copy) NSString *leftBtnStr;/// 左侧按钮名称 未设置默认“取消”
+@property (nonatomic, copy) NSString *rightBtnStr;/// 右侧按钮名称 未设置默认“确定”
+
 //是否有语音验证码
 @property (nonatomic, assign) BOOL isSpeechVerificationCode;
 
