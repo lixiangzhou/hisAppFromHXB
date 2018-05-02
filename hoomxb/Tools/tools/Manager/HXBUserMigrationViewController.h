1aeac4c3bbab06c5d8351f017946e594f998ef87
白迎春 <953152868@qq.com>
2018-05-02 19:31:11 +0800

    搭建用户迁移界面

---
+++ hoomxb/Tools/tools/Manager/HXBUserMigrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXBUserMigrationViewController.h
+//  hoomxb
+//
+//  Created by hxb on 2018/5/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBBaseViewController.h"
+
+@interface HXBUserMigrationViewController : HXBBaseViewController
+
+@end
---
+++ hoomxb/Tools/tools/Manager/HXBUserMigrationViewController.m
@@ -0,0 +1,37 @@
+//
+//  HXBUserMigrationViewController.m
+//  hoomxb
+//
+//  Created by hxb on 2018/5/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBUserMigrationViewController.h"
+
+@interface HXBUserMigrationViewController ()
+
+@end
+
+@implementation HXBUserMigrationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
