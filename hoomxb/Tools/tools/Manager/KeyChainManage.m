f74e2a685e5b78464ceffd3727c923dedfe24088
caihongji <cai_hongji@sina.cn>
2018-05-02 19:16:47 +0800

    no message

--- hoomxb/Tools/tools/Manager/KeyChainManage.m
+++ hoomxb/Tools/tools/Manager/KeyChainManage.m
@@ -217,7 +217,7 @@ static NSString *const hostH5 = @"hostH5";
         if(viewModel) {
             [self setValueWithUserInfoModel:viewModel];
             
-            BOOL isAccountActivation = YES;
+            BOOL isAccountActivation = NO;
             if(isAccountActivation) {//账户需要激活
                 [[HXBAccountActivationManager sharedInstance] entryActiveAccountPage];
                 if (resultBlock) {
