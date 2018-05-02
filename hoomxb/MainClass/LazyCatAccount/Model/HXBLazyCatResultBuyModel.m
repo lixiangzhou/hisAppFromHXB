c2bd72d57881cced2b106bc6146d55aaeb4c66c7
caihongji <cai_hongji@sina.cn>
2018-05-02 14:15:30 +0800

    添加充值， 购买， 提现对应的结果model

---
+++ hoomxb/MainClass/LazyCatAccount/Model/HXBLazyCatResultBuyModel.m
@@ -0,0 +1,13 @@
+//
+//  HXBLazyCatResultBuyModel.m
+//  hoomxb
+//
+//  Created by caihongji on 2018/5/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBLazyCatResultBuyModel.h"
+
+@implementation HXBLazyCatResultBuyModel
+
+@end
