38427703c8b856751d4d784c62559f4cddcfbd7a
caihongji <cai_hongji@sina.cn>
2018-05-07 18:05:40 +0800

    账户激活流程处理

--- hoomxb/Tools/tools/Manager/KeyChainManage.m
+++ hoomxb/Tools/tools/Manager/KeyChainManage.m
@@ -211,7 +211,7 @@ static NSString *const hostH5 = @"hostH5";
             [self setValueWithUserInfoModel:viewModel];
             
             BOOL isAccountActivation = viewModel.userInfoModel.userInfo.isUserActive;
-            if(!isAccountActivation) {//账户需要激活
+            if(!isAccountActivation && viewModel.userInfoModel.userInfo.isCreateEscrowAcc) {//账户需要激活
                 [[HXBAccountActivationManager sharedInstance] entryActiveAccountPage];
                 if (resultBlock) {
                     resultBlock(nil, [NSError errorWithDomain:@"" code:kHXBCode_AlreadyPopWindow userInfo:nil]);
