b233355ad4853807d3e444bb14f12cfd98f9e393
zhaicongcong <congcong.zhai@hoomsun.com>
2018-05-30 17:19:59 +0800

    在修改了tableViewHeatherView高度之后需要重新赋值tableHeaderView这个属性才能刷新cell与tableHeaderView的frame

--- hoomxb/Tools/tools/BaseURL/HXBBaseUrlManager.m
+++ hoomxb/Tools/tools/BaseURL/HXBBaseUrlManager.m
@@ -39,7 +39,7 @@
             if (storedBaseUrl.length > 0) {
                 _baseUrl = storedBaseUrl;
             } else {
-                _baseUrl = @"http://192.168.1.36:3100";
+                _baseUrl = @"http://192.168.1.37:3100";
             }
         }
     }
