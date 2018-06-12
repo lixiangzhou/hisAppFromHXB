66c0b30f62f1c447f066ed85a0238fd9aba859a7
baiyingchun <953152868@qq.com>
2018-06-12 15:13:54 +0800

    添加注释

--- hoomxb/MainClass/Home/HomePopView/Model/HXBHomePopVWModel.h
+++ hoomxb/MainClass/Home/HomePopView/Model/HXBHomePopVWModel.h
@@ -17,6 +17,6 @@
 @property (nonatomic, assign) long long updateTime;//弹窗数据更新时间
 @property (nonatomic, copy) NSString *type;//弹窗打开页面类型：native：app原生页面, h5：h5页面, broswer:页面在app打不开时，app用外部浏览器打开
 @property (nonatomic, copy) NSString *url;//弹窗跳转链接地址，不区分类型，可以是H5页面，也可以是原生页面
-@property (nonatomic, copy) NSString *frequency;//once:有效期内只打开一次，everyTime:有效期内每次启动都打开
+@property (nonatomic, copy) NSString *frequency;//弹窗展现频率。everyday:每天仅一次 ; everytime: 有效期内每次都展示；once: 只展示一次
 
 @end
