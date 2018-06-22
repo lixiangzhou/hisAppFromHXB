a704d57cc8b88eb711554a6d01954c7e5b611ada
zhaicongcong <congcong.zhai@hoomsun.com>
2018-06-22 16:12:51 +0800

    修改H5重新加载突然偏移的问题

--- hoomxb/Base/BaseController/WebViewController/HXBBaseWebViewController.m
+++ hoomxb/Base/BaseController/WebViewController/HXBBaseWebViewController.m
@@ -181,7 +181,7 @@
     
     [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.bottom.equalTo(weakSelf.view);
-        make.top.equalTo(weakSelf.progressView.mas_bottom);
+        make.top.equalTo(weakSelf.view).offset(HXBStatusBarAndNavigationBarHeight);
     }];
 }
 
