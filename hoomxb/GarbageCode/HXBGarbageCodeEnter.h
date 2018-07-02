27bd888f35e9f713c5aa830d5033a41bb15a6e32
caihongji <cai_hongji@sina.cn>
2018-07-02 13:53:20 +0800

    添加垃圾文件存放目录

---
+++ hoomxb/GarbageCode/HXBGarbageCodeEnter.h
@@ -0,0 +1,13 @@
+//
+//  HXBGarbageCodeEnter.h
+//  hoomxb
+//
+//  Created by caihongji on 2018/7/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXBGarbageCodeEnter : NSObject
+
+@end
---
+++ hoomxb/GarbageCode/HXBGarbageCodeEnter.m
@@ -0,0 +1,13 @@
+//
+//  HXBGarbageCodeEnter.m
+//  hoomxb
+//
+//  Created by caihongji on 2018/7/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBGarbageCodeEnter.h"
+
+@implementation HXBGarbageCodeEnter
+
+@end
