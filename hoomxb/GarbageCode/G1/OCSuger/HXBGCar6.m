da4ca0a554c0969c2894ddad7b84050b4a4b0132
李向洲 <lixiangzhouno1@163.com>
2018-07-02 18:02:43 +0800

    垃圾代码

---
+++ hoomxb/GarbageCode/G1/OCSuger/HXBGCar6.m
@@ -0,0 +1,13 @@
+//
+//  HXBGCar6.m
+//  hoomxb
+//
+//  Created by lxz on 2018/7/2.
+//  Copyright © 2018年 hoomsun-miniX. All rights reserved.
+//
+
+#import "HXBGCar6.h"
+
+@implementation HXBGCar6
+
+@end
